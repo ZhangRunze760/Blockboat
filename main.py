3d69d9ac0e1d7faa768d303c5c0944492a6e9bcf
ZhangRunze760 <weibo1960@outlook.com>
2023-01-30 06:18:38 +0800

    启用了多线程。

--- main.py
+++ main.py
@@ -24,6 +24,30 @@ MCUUID = MC['MCUUID']
 MCREMOTE_UUID = MC['MCREMOTE_UUID']
 MCAPIKEY = MC['MCAPIKEY']
 java_edition = MC['IsJavaEdition']
+app = Flask(__name__)
+
+
+@app.route('/', methods=["POST"])
+def post_data():
+    if request.get_json().get('group_id') == QQGroup_id:
+        with open('Bind.yaml', 'r', encoding='utf-8') as yaml_file:
+            bind_info = yaml.load(yaml_file, Loader=yaml.FullLoader)
+            list_of_qqid = list(bind_info.keys())
+
+        # 获取需要的消息
+        qqid = request.get_json().get('sender').get('user_id')  # 发送者的QQ号
+        if str(qqid) in list_of_qqid:
+            who = QQMCBind.look_for_mcid(qqid)
+        else:
+            who = request.get_json().get('sender').get('card')
+        message = request.get_json().get('raw_message')  # 发的什么东西
+
+        MessageProcessing.qq_message_processing(QQAPI, QQGroup_id, MCURL, MCUUID, MCREMOTE_UUID, MCAPIKEY,
+                                                message,
+                                                qqid, who, java_edition)
+        print("从HTTP POST上报器处成功接收上报，JSON字符串为：" + request.get_json())
+
+    return 'OK'  # 对go-cqhttp进行响应，不然会出现三次重试
 
 
 class MainThread(threading.Thread):
@@ -34,30 +58,6 @@ class MainThread(threading.Thread):
         self.counter = counter
 
     def run(self):  # 把要执行的代码写到run函数里面 线程在创建后会直接运行run函数
-        app = Flask(__name__)
-
-        @app.route('/', methods=["POST"])
-        def post_data():
-            if request.get_json().get('group_id') == QQGroup_id:
-                with open('Bind.yaml', 'r', encoding='utf-8') as yaml_file:
-                    bind_info = yaml.load(yaml_file, Loader=yaml.FullLoader)
-                    list_of_qqid = list(bind_info.keys())
-
-                # 获取需要的消息
-                qqid = request.get_json().get('sender').get('user_id')  # 发送者的QQ号
-                if str(qqid) in list_of_qqid:
-                    who = QQMCBind.look_for_mcid(qqid)
-                else:
-                    who = request.get_json().get('sender').get('card')
-                message = request.get_json().get('raw_message')  # 发的什么东西
-
-                MessageProcessing.qq_message_processing(QQAPI, QQGroup_id, MCURL, MCUUID, MCREMOTE_UUID, MCAPIKEY,
-                                                        message,
-                                                        qqid, who, java_edition)
-                print("从HTTP POST上报器处成功接收上报，JSON字符串为：" + request.get_json())
-
-            return 'OK'  # 对go-cqhttp进行响应，不然会出现三次重试
-
         # 开启我们的端口监听，监听我们设置好的端口，从而使得机器人框架向我们上报消息
         app.run(debug=True, host=QQURL, port=int(QQPort), threaded=True)
 
